428b8d76bad0399311f2c4c6e0621caa71c5fd81
zerozakiark <zerozakiark@gmail.com>
2015-10-14 19:08:13 +0800

    Create javaTime.java

---
+++ javaTime.java
@@ -0,0 +1,14 @@
+import java.time.*;
+//import java.time
+
+public class javaTime {
+  public static void main(String[] args) {
+    
+	//current date
+	LocalDate today = LocalDate.now();
+    
+	//output today
+    System.out.println(today);
+  
+  }
+}
